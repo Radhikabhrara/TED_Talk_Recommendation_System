baa410f7efe57e0a1507d9a19a8d55dd99fe60aa
Er. Radhika Bhrara <70060185+Radhikabhrara@users.noreply.github.com>
2023-05-20 00:24:54 +0530

    Update TTRS.py

--- TTRS.py
+++ TTRS.py
@@ -25,19 +25,13 @@ st.set_page_config(layout = "wide", page_title='Radhika_1917631')
 st.title("TED Talks Recommendation System")
 
 st.write('<p style="font-size:130%">Fetching Dataset</p>', unsafe_allow_html=True)
-
-file_format = st.radio('Select file format:', ('csv', 'excel'))
-data = st.file_uploader(label = '')
-
 data = 'TED_TALKS_DATA.csv'
 df = pd.read_csv(data)
-
-    
+data=df
 st.subheader('Dataframe:')
 n, m = df.shape
 st.write(f'<p style="font-size:130%">Dataset contains {n} rows and {m} columns.</p>', unsafe_allow_html=True)   
 st.dataframe(df)
-data=df
 
 #data = pd.DataFrame(data)
 #st.write(data.head())
@@ -50,18 +44,18 @@ def det(x):
         language = 'Other'
     return language
 df['language'] = df['Description'].apply(det)
-st.write(data)
+st.write(df)
 st.subheader("Filtering English language")
 filtered_for_english = df.loc[df['language'] == 'en']
 df = df[df['language'] == 'en']
-st.write(data)
+st.write(df)
 
-data['details'] = data["Title"] + ' ' + data['Description']
+df['details'] = df["Title"] + ' ' + df['Description']
  
 st.subheader("Removing the unnecessary information")
-#data = data[['main_speaker', 'details',"name","url","title","views"]]
-data.dropna(inplace = True)
-st.write(data.head())
+
+df.dropna(inplace = True)
+st.write(df.head())
 
 st.subheader("Removing stopwords")
 def remove_stopwords(text):
@@ -79,8 +73,8 @@ def remove_stopwords(text):
   output = " ".join(imp_words)
  
   return output
-data['details'] = data['details'].apply(lambda text: remove_stopwords(text))
-st.write(data.head())
+df['details'] = df['details'].apply(lambda text: remove_stopwords(text))
+st.write(df.head())
 
 punctuations_list = string.punctuation
 
@@ -90,16 +84,16 @@ def cleaning_punctuations(text):
 	return text.translate(signal)
 
 st.subheader("Cleaning punctuations")
-data['details'] = data['details'].apply(lambda x: cleaning_punctuations(x))
-st.write(data.head())
+df['details'] = df['details'].apply(lambda x: cleaning_punctuations(x))
+st.write(df.head())
 
-details_corpus = " ".join(data['details'])
+details_corpus = " ".join(df['details'])
 
 st.text("Training Model")
 vectorizer = TfidfVectorizer(analyzer = 'word')
-vectorizer.fit(data['details'])
+vectorizer.fit(df['details'])
 
-def get_similarities(talk_content, data=data):
+def get_similarities(talk_content, data=df):
 
 	# Getting vector for the input talk_content.
 	talk_array1 = vectorizer.transform(talk_content).toarray()
@@ -112,7 +106,7 @@ def get_similarities(talk_content, data=data):
 
 		# Getting vector for current talk.
 		talk_array2 = vectorizer.transform(
-			data[data['details'] == details]['details']).toarray()
+			df[df['details'] == details]['details']).toarray()
 
 		# Calculating cosine similarities
 		cos_sim = cosine_similarity(talk_array1, talk_array2)[0][0]
@@ -125,15 +119,15 @@ def get_similarities(talk_content, data=data):
 
 	return sim #, pea
 
-def recommend_talks(talk_content,n, data=data):
+def recommend_talks(talk_content,n, data=df):
  
-    data['cos_sim'] = get_similarities(talk_content)
+    df['cos_sim'] = get_similarities(talk_content)
  
-    data.sort_values(by='cos_sim', ascending=
+    df.sort_values(by='cos_sim', ascending=
                      False, inplace=True)
  
     
-    recommended_data = data.head(n)
+    recommended_data = df.head(n)
     recommended_data.sort_values(by=['Views'],ascending=False)
     r_pic = recommended_data[["Thumbnails"]]
     r_name = recommended_data[["Title"]]
