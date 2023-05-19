6069ff8363bc6a92d0c3b97022a3aaf2b660b0b1
Er. Radhika Bhrara <70060185+Radhikabhrara@users.noreply.github.com>
2023-05-19 17:00:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,13 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import sys
-del np.bool
+
 import tensorrt
 tensorrt.nptype(tensorrt.DataType.HALF)
 import io
 import PIL
 from PIL import Image
-
+import dask.dataframe as dd
 
 import difflib
 import nltk
@@ -34,7 +34,9 @@ st.subheader('Dataframe:')
 n, m = df.shape
 st.write(f'<p style="font-size:130%">Dataset contains {n} rows and {m} columns.</p>', unsafe_allow_html=True)   
    
-data=df
+data= df = dd.read_csv('TED_TALKS_DATA.csv')
+st.write(df)
+
 #data = pd.DataFrame(data)
 if 'numpy' in sys.modules:
     del sys.modules['numpy']
