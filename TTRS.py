40c6820f6df0d4be856883b8c74380c0709e36bc
Er. Radhika Bhrara <70060185+Radhikabhrara@users.noreply.github.com>
2023-05-24 00:09:41 +0530

    Update TTRS.py

--- TTRS.py
+++ TTRS.py
@@ -269,7 +269,7 @@ def recommend_talks(talk_content,n, data=df):
 		resized_image = image.resize(desired_size)
 		st.write("%s" %name)
 		st.image(resized_image, caption=cap)
-		markdown_code = f"[![image]({resized_image})]({id_url})"
+		markdown_code = f"[![({rsized_image}]]({id_url})"
 		# Render the markdown
 		st.markdown(markdown_code, unsafe_allow_html=True)
 		
