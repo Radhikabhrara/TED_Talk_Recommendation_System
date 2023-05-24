107fa6827a9febfe0d7c0343628baf06fe5de083
Er. Radhika Bhrara <70060185+Radhikabhrara@users.noreply.github.com>
2023-05-24 19:46:36 +0530

    Update TTRS.py

--- TTRS.py
+++ TTRS.py
@@ -157,53 +157,45 @@ st.write(f'<p style="font-size:130%">Dataset contains {n} rows and {m} columns.<
 st.dataframe(df)
 st.text("Processing data....")
 
-
-from langdetect import detect
-def det(x):
-    try:
-        language = detect(x)
-    except:
-        language = 'Other'
-    return language
-df['language'] = df['Description'].apply(det)
-
-df1=df
-
-
-filtered_for_english = df.loc[df['language'] == 'en']
-
+def process_data(df=df):
+	from langdetect import detect
+	def det(x):
+		try:
+			language = detect(x)
+		except:
+			language = 'Other'
+		return language
+	df['language'] = df['Description'].apply(det)
+	df1=df
+	filtered_for_english = df.loc[df['language'] == 'en']
+	df2 = df 
+	df['details'] = df["Title"] + ' ' + df['Description']
+	df.dropna(inplace = True)
+	df3=df
+	st.text("Few seconds away....")
+	def remove_stopwords(text):
+		stop_words = stopwords.words('english')
+		imp_words = []
+		# Storing the important words
+		for word in str(text).split():
+			word = word.lower()
+			if word not in stop_words:
+				imp_words.append(word)
+		output = " ".join(imp_words)
+		return output
+	d['details'] = df['details'].apply(lambda text: remove_stopwords(text))
+	df4=df
+	punctuations_list = string.punctuation
+	def cleaning_punctuations(text):
+		signal = str.maketrans('', '', punctuations_list)
+		return text.translate(signal)
+	df['details'] = df['details'].apply(lambda x: cleaning_punctuations(x))
+	df5=df
+	details_corpus = " ".join(df['details'])
+	retrurn df1,df2,df3,df4,df5,details_corpus
+	
+process_data(df)
 #df[df['language'] == 'en']
-df2 = df 
-
-
-df['details'] = df["Title"] + ' ' + df['Description']
-df.dropna(inplace = True)
-df3=df
-
-st.text("Few seconds away....")
-def remove_stopwords(text):
-  stop_words = stopwords.words('english')
-  imp_words = []
-  # Storing the important words
-  for word in str(text).split():
-    word = word.lower()
-    if word not in stop_words:
-      imp_words.append(word)
-  output = " ".join(imp_words)
-  return output
-
-d['details'] = df['details'].apply(lambda text: remove_stopwords(text))
-df4=df
-punctuations_list = string.punctuation
-def cleaning_punctuations(text):
-	signal = str.maketrans('', '', punctuations_list)
-	return text.translate(signal)
-
-#t.subheader("Cleaning punctuations")
-df['details'] = df['details'].apply(lambda x: cleaning_punctuations(x))
-df5=df
-
-details_corpus = " ".join(df['details'])
 
 st.sidebar.title("Menu Bar:")
 st.sidebar.header('Steps involved in Processing the data : 👉')
