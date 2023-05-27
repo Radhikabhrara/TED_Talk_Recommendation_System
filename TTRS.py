49ba29601f80c78c6c54351eb6d24c1fa177494e
Er. Radhika Bhrara <70060185+Radhikabhrara@users.noreply.github.com>
2023-05-27 18:36:32 +0530

    Update TTRS.py

--- TTRS.py
+++ TTRS.py
@@ -30,7 +30,9 @@ im = Image.open('logo.png')
 st.set_page_config(page_title="Radhika_1917631", page_icon = im ,layout="wide",initial_sidebar_state="auto",
 		   menu_items={'About': 'https://www.linkedin.com/in/radhika-bhrara/'})
 
-st.title("TED Talks Recommendation System")
+#st.title("TED Talks Recommendation System")
+st.markdown("<h1 style='text-align: center; color: red;'>TED Talks Recommendation System"</h1>, unsafe_allow_html=True)
+
 hide_default_format = """
        <style>
        #MainMenu {visibility: hidden; }
